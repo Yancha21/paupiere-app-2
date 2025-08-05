5ebf35fbfdbd2d4030cc96bee311545e5ef4af6e
Yancha21 <cby1996@yahoo.fr>
2025-08-05 11:07:14 +0200

    Add files via upload

---
+++ paupiere-app-4/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import PaupiereApp from './PaupiereApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaupiereApp />
+  </React.StrictMode>
+)
